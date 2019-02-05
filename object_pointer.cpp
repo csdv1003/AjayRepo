5949ceefd290a4be4219a9c4e71ac03fe736291e
AjayKumarTyagi <46739062+AjayKumarTyagi@users.noreply.github.com>
2019-02-05 22:17:43 +0530

    Add files via upload

---
+++ object_pointer.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class volume
+{
+	int h,b,l;
+	public:
+		volume()
+		{
+			cout<<"constructor called "<<endl;
+			h=10;
+			b=8;
+			l=9;
+		}
+		void vol()
+		{
+			int a;
+			a=l*b*h;
+			cout<<"volume= "<<a<<endl;
+		}
+};
+int main()
+{
+	volume v;
+	volume *ptr;
+	ptr=&v;
+	ptr->vol();
+}
